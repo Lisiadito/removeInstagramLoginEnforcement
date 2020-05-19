65885c6e65ac2455c5d1f9b43f519e8a0cc2a108
Patrick Weingärtner <patrick.weingaertner91@gmail.com>
2020-05-19 10:24:11 +0200

    minor formation fixes

--- content.js
+++ content.js
@@ -1,7 +1,11 @@
 window.addEventListener('load', event => {
     const observer = new MutationObserver(mutations => {
         mutations.forEach(mutation => {
-            if (mutation.addedNodes.length > 0 && mutation.addedNodes[0] !== undefined && mutation.addedNodes[0].attributes !== undefined && mutation.addedNodes[0].attributes.role !== undefined && mutation.addedNodes[0].attributes.role.value === 'presentation') {
+            if (mutation.addedNodes.length > 0 && 
+                mutation.addedNodes[0] !== undefined &&
+                mutation.addedNodes[0].attributes !== undefined && 
+                mutation.addedNodes[0].attributes.role !== undefined && 
+                mutation.addedNodes[0].attributes.role.value === 'presentation') {
 
                 mutation.addedNodes[0].remove()
             }
